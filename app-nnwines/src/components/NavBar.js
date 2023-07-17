d1ae32d53f13057f47c66120d6f35c72a36b1c60
mateotardini <mateotardini@gmail.com>
2023-07-17 13:09:22 -0300

    Inicio de estetica responsive

--- app-nnwines/src/components/NavBar.js
+++ app-nnwines/src/components/NavBar.js
@@ -1,81 +1,98 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { HiOutlineShoppingCart,HiLogout,HiLogin,HiUpload,HiOutlineViewGrid,HiOutlineHome,HiOutlineInformationCircle } from 'react-icons/hi';
+import { HiOutlineShoppingCart, HiLogout, HiLogin, HiUpload, HiOutlineViewGrid, HiOutlineHome, HiOutlineInformationCircle, HiX, HiMenu } from 'react-icons/hi';
 import styles from '../css/NavBar.module.css';
 import { CartContext } from '../contexts/ShoppingCartContext';
 import { AuthContext } from '../contexts/AuthContext.js';
 import ShoppingCart from './ShoppingCart.js';
 
 function NavBar() {
-    const { isAuthenticated, handleLogout } = useContext(AuthContext);
-    const [shoppingCart, setShoppingCart, addToShoppingCart, removeToShoppingCart, openCart, handleOpenCart] = useContext(
-        CartContext
-    );
+  const { isAuthenticated, handleLogout } = useContext(AuthContext);
+  const [shoppingCart, setShoppingCart, addToShoppingCart, removeToShoppingCart, openCart, handleOpenCart] = useContext(CartContext);
 
-    const totalProductsQuantity = shoppingCart.reduce((acc, curr) => {
-        return acc + curr.quantity;
-    }, 0);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const handleLogoutClick = () => {
-        handleLogout(); // Call the logout function from AuthContext
-        // Additional cleanup or logic after logout if needed
-    };
+  const totalProductsQuantity = shoppingCart.reduce((acc, curr) => {
+    return acc + curr.quantity;
+  }, 0);
 
-    return (
-        <div className={styles.container}>
+  const handleLogoutClick = () => {
+    handleLogout();
+  };
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
+
+  const handleGoToCart = () => {
+    handleMenuClose();
+    handleOpenCart();
+  };
+
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.hamburgerContainer} onClick={handleMenuToggle}>
+        <HiMenu />
+      </div>
+
+      <div className={`${styles.container} ${menuOpen ? styles.menuOpen : ''}`}>
+        <button className={styles.closeButton} onClick={handleMenuClose}>
+          <HiX />
+        </button>
+
+        <Link to="/">
+          <img className={styles.logo} src={require(`../images/NN WINES LOGO.png`)} alt="Logo NN Wines" />
+        </Link>
+
+        <ul className={styles.menuNavBar}>
+          <li>
             <Link to="/">
-            <img
-                className={styles.logo}
-                src={require(`../images/NN WINES LOGO.png`)}
-                alt="Logo NN Wines"
-            />
+              <div className={styles.menuButton}><HiOutlineHome /> inicio</div>
             </Link>
+          </li>
+          <li>
+            <Link to="/Products">
+              <div className={styles.menuButton}><HiOutlineViewGrid /> productos</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/Como-Comprar">
+              <div className={styles.menuButton}><HiOutlineInformationCircle /> cómo comprar</div>
+            </Link>
+          </li>
+          <li>
+            <button className={styles.menuButton} onClick={handleGoToCart}>
+              <HiOutlineShoppingCart /> ({totalProductsQuantity})
+            </button>
+          </li>
 
-            <ul className={styles.menuNavBar}>
-                <li>
-                    <Link to="/">
-                        <div className={styles.menuButton}><HiOutlineHome/>  inicio</div>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/Products">
-                        <div className={styles.menuButton}><HiOutlineViewGrid />  productos</div>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/Como-Comprar">
-                        <div className={styles.menuButton}><HiOutlineInformationCircle/> cómo comprar</div>
-                    </Link>
-                </li>
-                <li>
-                    <button className={styles.menuButton} onClick={() => handleOpenCart()}>
-                        <HiOutlineShoppingCart /> ({totalProductsQuantity})
-                    </button>
-                </li>
-
-                {isAuthenticated ? (
-                    <>
-                        <li>
-                            <Link to="/QqGEDmDXqXaQ">
-                                <div className={styles.menuButton}><HiUpload />  Subir Producto</div>
-                            </Link>
-                        </li>
-                        <li>
-                            <button className={styles.menuButton} onClick={handleLogoutClick}>
-                                <HiLogout />  Log Out
-                            </button>
-                        </li>
-                    </>
-                ) : (
-                    <Link to="/login">
-                        <div className={styles.menuButton}><HiLogin/>  Iniciar Sesión</div>
-                    </Link>
-                )}
-
-            </ul>
-            <ShoppingCart />
-        </div>
-    );
+          {isAuthenticated ? (
+            <>
+              <li>
+                <Link to="/QqGEDmDXqXaQ">
+                  <div className={styles.menuButton}><HiUpload /> Subir Producto</div>
+                </Link>
+              </li>
+              <li>
+                <button className={styles.menuButton} onClick={handleLogoutClick}>
+                  <HiLogout /> Log Out
+                </button>
+              </li>
+            </>
+          ) : (
+            <Link to="/login">
+              <div className={styles.menuButton}><HiLogin /> Iniciar Sesión</div>
+            </Link>
+          )}
+        </ul>
+        <ShoppingCart />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
